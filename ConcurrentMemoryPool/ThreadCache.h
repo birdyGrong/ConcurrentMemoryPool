cfc36bc42ba7008380908880ccf2be2f0b6028fe
GrongYH <952286890@qq.com>
2022-03-10 23:21:32 +0800

    使用基数树对内存池性能进行了优化。使用基数树对内存池性能进行了优化添加项目文件。

---
+++ ConcurrentMemoryPool/ThreadCache.h
@@ -0,0 +1,28 @@
+#pragma once
+#include"Common.h"
+
+
+// ThreadCache是每个线程独有的，用于小于256KB内存的分配。
+// 当线程需要小于等于256KB的内存时，会直接从ThreadCache申请。
+
+
+class ThreadCache
+{
+public:
+	//申请内存
+	void* Allocate(size_t size);
+
+	//释放内存
+	void Deallocate(void* ptr, size_t size);
+
+	//从中心缓存获取对象
+	void* FetchFromCentralCache(size_t index, size_t size);
+
+	//释放对象时，链表过长时，回收内存回到中心缓存
+	void ListTooLong(FreeList& list, size_t size);
+
+private:
+	FreeList _freeList[NFREELISTS];
+};
+
+static _declspec(thread) ThreadCache* tls_threadcache = nullptr;
